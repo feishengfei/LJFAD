1314e6a11b658e3d45960a3e16cb9e27016cfb26
Frank Zhou <feishengfei@126.com>
2013-10-03 09:52:40 +0800

    listing 3-9 Logically comparing Point objects

---
+++ src/listing3_1/Point.java
@@ -0,0 +1,47 @@
+package listing3_1;
+
+public class Point {
+	private int x, y;
+
+	public Point(int x, int y) {
+		super();
+		this.x = x;
+		this.y = y;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (!(o instanceof Point)) {
+			return false;
+		}
+		Point p = (Point)o;
+		return p.x == x && p.y == y;
+	}
+	
+	public static void main(String[] args) {
+		Point p1 = new Point(10, 20);
+		Point p2 = new Point(20, 30);
+		Point p3 = new Point(10, 20);
+		//Test reflexivity
+		System.out.println(p1.equals(p1));
+		//Test symmetry
+		System.out.println(p1.equals(p2));
+		System.out.println(p2.equals(p1));
+		//Test transitivity
+		System.out.println(p2.equals(p3));
+		System.out.println(p1.equals(p3));
+		//Test nullability
+		System.out.println(p1.equals(null));
+		//Extra test to further prove the instanceof operator's usefullness.
+		System.out.println(p1.equals("abc"));
+	}
+	
+}
