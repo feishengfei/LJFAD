197703d5c0c2a588a5c83234bc8f892585f99f46
Frank Zhou <feishengfei@126.com>
2013-10-04 07:31:06 +0800

    listing 4-4~5 Demo nonstatic member class

--- src/listing4_1/EnclosingClass.java
+++ src/listing4_1/EnclosingClass.java
@@ -1,29 +1,21 @@
 package listing4_1;
 
 public class EnclosingClass {
-	private static int i;
+	private int i;
 	
-	private static void m1() {
+	private void m1() {
 		System.out.println(i);
 	}
 	
-	public static void m2() {
-		EnclosedClass.accessEnclosingClass();
-	}
-	
-	public static class EnclosedClass {
-		public static void accessEnclosingClass() {
+	public class EnclosedClass {
+		public void accessEnclosingClass() {
 			i = 1;
 			m1();
-		}
-		public void accessEnclosingClass2() {
-			m2();
 		}
 	}
 	
 	public static void main(String[] args) {
-		EnclosingClass.EnclosedClass.accessEnclosingClass();
-		EnclosingClass.EnclosedClass ec = new EnclosingClass.EnclosedClass();
-		ec.accessEnclosingClass2();
+		EnclosingClass ec = new EnclosingClass();
+		ec.new EnclosedClass().accessEnclosingClass();
 	}
 }
