f7a997814ec8104a89140d10c37e4c8765e030e7
Frank Zhou <feishengfei@126.com>
2013-10-04 09:18:10 +0800

    listing 4-12 Declaring a Local Class

--- src/listing4_1/EnclosingClass.java
+++ src/listing4_1/EnclosingClass.java
@@ -1,21 +1,19 @@
 package listing4_1;
 
 public class EnclosingClass {
-	private int i;
-	
-	private void m1() {
-		System.out.println(i);
-	}
-	
-	public class EnclosedClass {
-		public void accessEnclosingClass() {
-			i = 1;
-			m1();
+	private void m(final int x) {
+		final int y = x*2;
+		class LocalClass {
+			int a = x;
+			int b = y;
 		}
+		LocalClass lc = new LocalClass();
+		System.out.println(lc.a);
+		System.out.println(lc.b);
 	}
 	
 	public static void main(String[] args) {
 		EnclosingClass ec = new EnclosingClass();
-		ec.new EnclosedClass().accessEnclosingClass();
+		ec.m(1);
 	}
 }
