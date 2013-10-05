cb7ca38473f029aafd38ff43db86dcb4301e0018
Frank Zhou <feishengfei@126.com>
2013-10-05 09:26:08 +0800

    listing 4-31 DemoFinally in File Copy(Cleaning up before handling thrown exception)

--- src/listing4_1/DemoFinally.java
+++ src/listing4_1/DemoFinally.java
@@ -8,55 +8,51 @@ import java.io.IOException;
 public class DemoFinally
 {
 	public static void main(String[] args) {
-		if(args.length != 2) {
+		if (args.length != 2) {
 			System.err.println("usage: java Copy srcfile dstfile");
 			return;
 		}
-		FileInputStream fis = null;
 		try {
-			fis = new FileInputStream(args[0]);
-			FileOutputStream fos = null;
-			try {
-				fos = new FileOutputStream(args[1]);
-				int b;
-				while((b = fis.read()) != -1) {
-					fos.write(b);
-				}
-			} 
-			catch (FileNotFoundException fnfe) {
-				String msg = args[1] + " could not be created, possibly because "
-				+ "it might be a directory";
-				System.err.println(msg);
-			} 
-			catch (IOException ioe) {
-				String msg = args[0] + " could not be read, or "
-				+ args[1] + " could not be written";
-				System.err.println(msg);
-			} 
-			finally {
-				if (fos != null) {
-					try {
-						fos.close();
-					} catch (IOException ioe) {
-						System.err.println("unable to close " + args[1]);
-					}
-				}
-			}
-		
-			
-		} 
+			copy(args[0], args[1]);
+		}
 		catch (FileNotFoundException fnfe) {
-			String msg = args[0] + " could not be found or might be a directory";
+			String msg = args[0] + " could not be found or might be a directory,"
+			+ " or " + args[1] + " could not be created, " 
+			+ "possibly because " + args[1] + " is a directory";
 			System.err.println(msg);
 		}
-		finally {
-				if (fis != null) {
-					try {
-						fis.close();
-					} catch (IOException ioe) {
-						System.err.println("unable to close " + args[0]);
-					}
+		catch (IOException ioe) {
+			String msg = args[0] + " could not be read, or "
+				+ args[1] + " could not be written";
+			System.err.println(msg);
+		}
+	}
+	public static void copy(String srcFile, String dstFile) throws IOException {
+		FileInputStream fis = new FileInputStream(srcFile);
+		try {
+			FileOutputStream fos = new FileOutputStream(dstFile, true);
+			try {
+				int b;
+				while ((b = fis.read()) != -1) {
+					fos.write(b);
 				}
+			}
+			finally {
+				try {
+					fos.close();
+				} 
+				catch (IOException e) {
+					System.err.println("unable to close " + dstFile);
+				}
+			}
+		}
+		finally {
+			try {
+				fis.close();
+			} 
+			catch (IOException e) {
+				System.err.println("unable to close " + dstFile);
+			}
 		}
 		
 	}
