9eb7b2f6ee7849d8746e955bebad6aacc82efe8a
Frank Zhou <feishengfei@126.com>
2013-10-06 09:20:41 +0800

    listing 5-19 Annotating undesirable application elements

--- src/listing5_1/Deck.java
+++ src/listing5_1/Deck.java
@@ -1,13 +1,21 @@
 package listing5_1;
 
+@Stub("1,10/21/2010,unassigned")
 public class Deck
 {
-	@Stub(value = "1,10/21/2010,unassigned")
+	@Stub("2,10/21/2010,unassigned")
+	private Card[] cardsRemaining;
+
+	@Stub("3,10/21/2010,unassigned")
+	public Deck(){
+	}
+	
+	@Stub("4,10/21/2010,unassigned")
 	public void shuffle() {
 	}
 	
-	@Stub("2,10/21/2010,unassigned")
-	public Card[] deal(int ncards) {
+	@Stub("5,10/21/2010,unassigned")
+	public Card[] deal(@Stub("5,10/21/2010,unassigned") int ncards) {
 		return null;
 	}
 	
