64a777bc1bf69d735b6380077cdd150a0a00d980
Frank Zhou <feishengfei@126.com>
2015-01-24 22:23:32 +0800

    add IntegerLongShortByteTest

---
+++ src/listing6/IntegerLongShortByteTest.java
@@ -0,0 +1,30 @@
+package listing6;
+
+public class IntegerLongShortByteTest
+{
+
+	public static void main(String[] args)
+	{
+		System.out.println("Long.MAX_VALUE: " + Long.MAX_VALUE);
+		System.out.println("Long.MIN_VALUE: " + Long.MIN_VALUE);
+		System.out.println("Long.SIZE: " + Long.SIZE);
+		System.out.println();
+		
+		System.out.println("Integer.MAX_VALUE: " + Integer.MAX_VALUE);
+		System.out.println("Integer.MIN_VALUE: " + Integer.MIN_VALUE);
+		System.out.println("Integer.SIZE: " + Integer.SIZE);
+		System.out.println();
+		
+		System.out.println("Short.MAX_VALUE: " + Short.MAX_VALUE);
+		System.out.println("Short.MIN_VALUE: " + Short.MIN_VALUE);
+		System.out.println("Short.SIZE: " + Short.SIZE);
+		System.out.println();
+		
+		System.out.println("Byte.MAX_VALUE: " + Byte.MAX_VALUE);
+		System.out.println("Byte.MIN_VALUE: " + Byte.MIN_VALUE);
+		System.out.println("Byte.SIZE: " + Byte.SIZE);
+		System.out.println();
+		
+	}
+
+}
