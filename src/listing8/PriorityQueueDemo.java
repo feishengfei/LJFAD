cff13521b9ceae719b777e0aec11ea890c7468d0
Frank Zhou <feishengfei@126.com>
2015-02-07 21:54:29 +0800

    Listing 8–19. Using a comparator with a priority queue

--- src/listing8/PriorityQueueDemo.java
+++ src/listing8/PriorityQueueDemo.java
@@ -1,15 +1,27 @@
 package listing8;
 
+import java.util.Comparator;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class PriorityQueueDemo
 {
-
+	final static int NELEM = 15;
+	
 	public static void main(String[] args)
 	{
-		Queue<Integer> qi = new PriorityQueue<Integer>();
-		for (int i = 0; i < 15; i++)
+		Comparator<Integer> cmp;
+		cmp = new Comparator<Integer>()
+		{
+
+			@Override
+			public int compare(Integer o1, Integer o2)
+			{
+				return o2 - o1;
+			}
+		};
+		Queue<Integer> qi = new PriorityQueue<Integer>(NELEM, cmp);
+		for (int i = 0; i < NELEM; i++)
 			qi.add((int) (Math.random()*100));
 		while(!qi.isEmpty())
 			System.out.print(qi.poll() + " ");
